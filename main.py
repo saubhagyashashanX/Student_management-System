69af86d94c0c70e0c19c10fa5167453f4e06878a
Saubhagya Shashank <saubhagyashashank17@gmail.com>
2026-06-30 00:10:01 +0530

    new features

--- main.py
+++ main.py
@@ -9,27 +9,43 @@ def show_menu():
     print("6. Exit")
 
 students=[]
-    
-while True:
-    show_menu()
-    user_input=input("Enter your choice: ")
-    if user_input=="1":
-        print("Adding Student...")
-        name=input("Enter student name: ")
-        roll=input("Enter student roll number: ")
-        age=input("Enter student age: ")
-        marks=input("Enter student marks: ")
-        student={
+def add_student():
+    name=input("Enter student name: ")
+    roll=input("Enter student roll number: ")
+    age=input("Enter student age: ")
+    marks=input("Enter student marks: ")
+    student={
             "name": name,
             "roll": roll,
             "age": age,
             "marks": marks
         }
-        students.append(student)
+    students.append(student)
+def view_student():
+    if len(students)==0:
+        print("No students found.")
+    else:
+        for student in students:
+            print(f"Name: {student['name']}, Roll: {student['roll']}, Age: {student['age']}, Marks: {student['marks']}")
+def search_student():
+    Id=input("Enter the roll")
+    found=False 
+    for student in students:
+        if Id==student['roll']:
+            print(f"Name: {student['name']}, Roll: {student['roll']}, Age: {student['age']}, Marks: {student['marks']}")
+            found=True
+    if not found:
+        print("Invalid Choice")
+
+while True:
+    show_menu()
+    user_input=input("Enter your choice: ")
+    if user_input=="1":
+        add_student()
     elif user_input=="2":
-        print("Viewing Student...")
+        view_student()
     elif user_input=="3":
-        print("Searching Student...")
+        search_student()
     elif user_input=="4":
         print("Updating Student...")
     elif user_input=="5":
