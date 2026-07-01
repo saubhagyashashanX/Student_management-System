abc58ee3607358ca9842d57504a0d91c553da00a
Saubhagya Shashank <saubhagyashashank17@gmail.com>
2026-07-02 00:44:37 +0530

    new features

--- fileHandler.py
+++ fileHandler.py
@@ -1,5 +1,19 @@
-def load_students():
-    pass
-def save_students():
-    pass
+def load_students(students):
+    with open("students.txt","r") as file:
+        for line in file:
+            line=line.strip()
+            data=line.split(",")
+            student={
+                "name":data[0],
+                "roll":data[1],
+                "age":data[2],
+                "marks":data[3]
+            }
+            students.append(student)
+    return students
+    
+def save_students(students):
+    with open("students.txt","w") as file:
+        for student in students:
+            file.write(f"{student['name']},{student['roll']},{student['age']},{student['marks']}")
     
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from student import *
+from fileHandler import save_students,load_students
 def show_menu():
     print("Student Management System")
     print("1. Add Student")
@@ -9,19 +10,25 @@ def show_menu():
     print("6. Exit")
 
 students=[]
+students=load_students()
 while True:
     show_menu()
     user_input=input("Enter your choice: ")
     if user_input=="1":
         add_student(students)
+        save_students(students)
     elif user_input=="2":
         view_student(students)
+        save_students(students)
     elif user_input=="3":
         search_student(students)
+        save_students(students)
     elif user_input=="4":
         update_student(students)
+        save_students(students)
     elif user_input=="5":
         delete_student(students)
+        save_students(students)
     elif user_input=="6":
         break
     else:
